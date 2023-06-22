40220dac8749f569d66f05e99d0a4fcdae10c231
Lee1387 <127062297+Lee1387@users.noreply.github.com>
2023-06-22 01:01:54 +0100

    added AnnouncerCommand.php

---
+++ src/Lee1387/announcer/Commands/AnnouncerCommand.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace Lee1387\announcer\Commands;
+
+use pocketmine\command\Command;
+use pocketmine\command\CommandSender;
+use pocketmine\plugin\PluginOwned;
+use pocketmine\utils\TextFormat;
+use Lee1387\announcer\Announcer;
+
+class AnnouncerCommand extends Command implements PluginOwned {
+
+    /** @var Announcer */
+    private Announcer $plugin;
+
+    public function __construct(Announcer $plugin) {
+        parent::__construct("announcer", "Reload the Announcer configuration", "/autoannouncer", ["aa"]);
+        $this->setPermission("autoannounce.reload");
+        $this->plugin = $plugin;
+    }
+
+    public function execute(CommandSender $sender, string $commandLabel, array $args): bool 
+    {
+        if(!$sender->hasPermission("autoannounce.reload")) {
+            $sender->sendMessage(TextFormat::RED . "You do not have sufficient permission to use this command!");
+            return false;
+        }
+
+        $this->plugin->reloadConfig();
+        $sender->sendMessage(TextFormat::GREEN . "Successfully reloaded configuration.");
+        return true;
+    }
+
+    public function getOwningPlugin(): Announcer
+    {
+        return $this->plugin;
+    }
+}
